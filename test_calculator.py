e249618519cca17d9f0a626249ebb86d3982977b
Jordan <jordan.bamber@gmail.com>
2021-06-12 09:34:17 -0700

    fixed linting erros

--- test_calculator.py
+++ test_calculator.py
@@ -4,14 +4,14 @@ Tests for calc app
 
 import calculator
 
+
 class TestCalculatorApp:
-    
+
     def test_add(self):
         assert 5 == calculator.add(3, 2)
-        
+
     def test_subtract(self):
         assert 5 == calculator.subtract(10, 5)
-        
+
     def test_multiply(self):
         assert 4 == calculator.multiply(2, 2)
-        
